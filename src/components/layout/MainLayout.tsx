fa328d069382cf1bc94baa9ba2d9430d39b16124
rabby-web <rabby.webdeveloper@gmail.com>
2025-01-06 22:58:51 +0600

    feat: Set up routing with react-router-dom
    - Added a router using createBrowserRouter from react-router-dom.
    - Configured the root path  to render the App component.
    - Exported the router for integration into the application.

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,24 +1,34 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { createElement } from "react";
-
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: createElement(icon),
-  label: `nav ${index + 1}`,
-}));
-
+const items: MenuProps["items"] = [
+  {
+    key: "as1",
+    label: "Dashboard",
+  },
+  {
+    key: "as2",
+    label: "Profile",
+  },
+  {
+    key: "as3",
+    label: "User Management",
+    children: [
+      {
+        key: "as31",
+        label: "Create Admin",
+      },
+      {
+        key: "as32",
+        label: "Create Faculty",
+      },
+      {
+        key: "as33",
+        label: "Create Student",
+      },
+    ],
+  },
+];
 export default function MainLayout() {
   return (
     <>
@@ -33,7 +43,17 @@ export default function MainLayout() {
             console.log(collapsed, type);
           }}
         >
-          <div className="demo-logo-vertical" />
+          <div
+            style={{
+              color: "white",
+              height: "4rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1>PH University</h1>
+          </div>
           <Menu
             theme="dark"
             mode="inline"
