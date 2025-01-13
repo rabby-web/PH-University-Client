5e6896933db649a29dac44a5a41dfc212b8b7f4d
rabby-web <rabby.webdeveloper@gmail.com>
2025-01-13 13:52:38 +0600

    feat: implement login form with user authentication logic using React Hook Form and Redux Toolkit (#123)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,39 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 export default function Login() {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data", data);
+  console.log("error", error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(userInfo);
+  };
   return (
     <>
-      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="id">ID:</label>
+          <input type="text" id="id" {...register("id")} />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" {...register("password")} />
+        </div>
+        <Button htmlType="submit">Login</Button>
+      </form>
     </>
   );
 }
