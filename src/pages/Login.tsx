ed4f7a6df16b9220db8b46661062932a16502f26
rabby-web <rabby.webdeveloper@gmail.com>
2025-01-13 14:30:39 +0600

    Implement login functionality with token verification and dispatch user data

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 export default function Login() {
   const { register, handleSubmit } = useForm({
@@ -10,17 +13,22 @@ export default function Login() {
     },
   });
 
-  const [login, { data, error }] = useLoginMutation();
-  console.log("data", data);
-  console.log("error", error);
+  const [login, { error }] = useLoginMutation();
+  const dispatch = useAppDispatch();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.id,
       password: data.password,
     };
-    login(userInfo);
+
+    const res = await login(userInfo).unwrap();
+
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
+
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
