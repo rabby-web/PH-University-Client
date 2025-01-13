6659465c65ba42c76fee0abc7c3cd3cf50d85709
rabby-web <rabby.webdeveloper@gmail.com>
2025-01-13 17:02:11 +0600

    Add TUser type definition, implement logout functionality, and integrate Toaster for notifications

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export default function Login() {
+  const navigate = useNavigate();
   const { register, handleSubmit } = useForm({
     defaultValues: {
       id: "A-0001",
@@ -13,20 +17,27 @@ export default function Login() {
     },
   });
 
-  const [login, { error }] = useLoginMutation();
+  const [login] = useLoginMutation();
   const dispatch = useAppDispatch();
 
-  const onSubmit = async (data) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
+  const onSubmit = async (data: FieldValues) => {
+    const tostId = toast.loading("Loading");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
 
-    const res = await login(userInfo).unwrap();
+      const res = await login(userInfo).unwrap();
 
-    const user = verifyToken(res.data.accessToken);
+      const user = verifyToken(res.data.accessToken) as TUser;
 
-    dispatch(setUser({ user: user, token: res.data.accessToken }));
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      navigate(`/${user.role}/dashboard`);
+      toast.success("Login", { id: tostId, duration: 2000 });
+    } catch (err) {
+      toast.error("Something went wrong", { id: tostId, duration: 2000 });
+    }
   };
 
   return (
