06c0f2e1b6bb6508dc07dc43f8d869b7e6050893
rabby-web <rabby.webdeveloper@gmail.com>
2025-01-15 15:06:48 +0600

    project commit by kashem

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button, Row } from "antd";
-import { FieldValues, useForm, useFormContext } from "react-hook-form";
+import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser, TUser } from "../redux/features/auth/authSlice";
@@ -19,6 +18,11 @@ export default function Login() {
   //   },
   // });
 
+  const defaultValues = {
+    id: "A-0001",
+    password: "admin123",
+  };
+
   const [login] = useLoginMutation();
   const dispatch = useAppDispatch();
 
@@ -28,7 +32,7 @@ export default function Login() {
 
     try {
       const userInfo = {
-        id: data.userId,
+        id: data.id,
         password: data.password,
       };
       const res = await login(userInfo).unwrap();
@@ -43,7 +47,7 @@ export default function Login() {
 
   return (
     <Row justify="center" align="middle" style={{ height: "100vh" }}>
-      <PHFrom onSubmit={onSubmit} >
+      <PHFrom onSubmit={onSubmit} defaultValues={defaultValues}>
         <PHInput type="text" name="userId" label="ID:" />
         <PHInput type="text" name="password" label="Password" />
         <Button htmlType="submit">Login</Button>
