7cb28e47265572b9304f383131b5f3eca5b9ce74
rabby-web <rabby.webdeveloper@gmail.com>
2025-01-23 19:21:39 +0600

    refactore code

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
   logout,
-  selectCurrentUser,
   useCurrentToken,
 } from '../../redux/features/auth/authSlice';
 import { Navigate } from 'react-router-dom';
